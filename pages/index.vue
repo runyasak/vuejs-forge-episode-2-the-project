511577ff013f563bbcf2da937d0e635e51f1c3d1
Runyasak Chaengnaimuang <world@odds.team>
2022-08-31 01:01:08 +0700

    feat: implement TransitionGroup on IndexPage

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,36 @@ useAsyncData("products", async () => productStore.fetchProducts());
       v-if="productStore.products"
       class="gap-7 p-10 sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-wrap justify-items-stretch items-stretch"
     >
-      <ProductCard
-        v-for="product in productStore.products"
-        :product="product"
-        :key="product.sys.id"
-        class="mb-5"
-      />
+      <TransitionGroup name="products">
+        <ProductCard
+          v-for="product in productStore.products"
+          :product="product"
+          :key="product.sys.id"
+          class="mb-5"
+        />
+      </TransitionGroup>
     </div>
   </div>
 </template>
+
+<style>
+.product-card {
+  transition: all 0.5s ease-in-out;
+}
+
+.products-enter-form {
+  transform: scale(0.5) translateY(80px);
+  opacity: 0;
+}
+
+.products-leave-to {
+  transform: translateY(30px);
+  opacity: 0;
+}
+
+.products-leave-active {
+  position: absolute;
+  z-index: -1;
+  max-width: 300px;
+}
+</style>

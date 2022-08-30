0f76f78ef8bf06ebb1aa8ac059a2d0376979f580
Runyasak Chaengnaimuang <world@odds.team>
2022-08-31 04:36:56 +0700

    feat: implement dbRestRequest on updateCart

--- composables/UseDeskree.js
+++ composables/UseDeskree.js
@@ -108,15 +108,9 @@ export function useDeskree() {
 
       // persist user's cart data to Deskree here
 
-      // example of what the return from Deskree will look like
-      return {
-        data: {
-          author: "4xsOPtHHiSMI06OHT5gvDnwmLuo2",
-          createdAt: "2022-08-19T06:24:47-05:00",
-          products: JSON.parse("[]"),
-          updatedAt: "2022-08-22T11:03:07-05:00",
-        },
-      };
+      return dbRestRequest(`/carts/${loggedInUser.value.cartId}`, "PATCH", {
+        products: JSON.stringify(products),
+      });
     },
     async getCart() {
       // get the user's persisted cart from Deskree here
